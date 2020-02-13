407619d2589147db90c8184b216a3660209c5e85
Joe Hendy <jahendy12@gmail.com>
2020-02-13 12:31:05 -0700

    anotha one

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,25 +1,36 @@
 import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
+
+import logo from '../../images/logos/Headphones.png'
 
 function NavBar() {
     return (
-<nav>       
-    <div className="#1e88e5 blue darken-1 container">
-        <div className="row">
-            <div className="col s12">
-                <div className="nav-wrapper">
-                    <a href="/" className="brand-logo">VibeTune (Logo)</a>
-                        <ul id="nav-mobile" className="right hide-on-med-and-down">
-                            <li><a href="/playlistList">Playlists</a></li>
-                            <li><a href="/liveroomlist">Live Rooms</a></li>
-                            <li><a href="/userprofile">Profile</a></li>
-                            <li><a href="/sign-in">Sign In</a></li>
-                        </ul>
+        <nav className="blue darken-1">
+            <div className="container">
+                <div className="row">
+                    <div className="col s12">
+                        <div className="brand-logo">
+                            <img src={logo} alt='headphones' className="logo" />
+                        </div>
+                    </div>
+                    <ul className="right hide-on-med-and-down">
+                        <li>
+                            <Link to="/playlistList">Playlists</Link>
+                        </li>
+                        <li>
+                            <Link to="/liveroomlist">Live Rooms</Link>
+                        </li>
+                        <li>
+                            <Link to="/userprofile">Profile</Link>
+                        </li>
+                        <li>
+                            <Link to="/sign-in">Sign In</Link>
+                        </li>
+                    </ul>
                 </div>
             </div>
-        </div>
-    </div>
-</nav>     
-)
+        </nav>
+    )
 }
 
 export default NavBar
